792b50a2776c5a22dffd806a3d90b1f7ba2fa4cc
a.polischuk <a.polischuk@adriver.ru>
2023-11-01 13:57:30 +0300

    feat: create app.js

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/App.css";
+import { Navbar } from "./components/UI/navbar/Navbar";
+import { AppRouter } from "./components/AppRouter";
+import { AuthContext } from "./context";
+
+function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("auth")) {
+      setIsAuth(true);
+    }
+  }, []);
+
+  return (
+    <div>
+      <AuthContext.Provider value={
+        {
+          isAuth,
+          setIsAuth
+        }
+      }>
+        <BrowserRouter>
+          <Navbar />
+          <AppRouter />
+      </BrowserRouter>
+      </AuthContext.Provider>
+    </div>
+  )
+}
+
+export default App;
